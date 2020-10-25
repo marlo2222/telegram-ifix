6a5a59adddf68938c31eb40af857f2baeda4a6f3
marlo2222 <marlo_henrique@hotmail.com.br>
2020-10-25 11:41:28 -0300

    iniciando o repositorio

---
+++ src/test/java/com/telegramifix/TelegramIfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegramifix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramIfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
